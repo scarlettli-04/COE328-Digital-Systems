256fd14cda8bd122ebedf9199f9fa6fbd13e1a30
Scarlett Li <168252605+scarlettli-04@users.noreply.github.com>
2024-11-28 01:04:09 -0500

    Create registerB.vhd

---
+++ lab6/registerB.vhd
@@ -0,0 +1,20 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+entity registerB IS
+	port (		B	:	in    std_logic_vector(7 downto 0) ; -- 8-bit A input 
+		reset, clk	:	in    std_logic ; 
+					Q	:	out  std_logic_vector(7 downto 0)) ; -- 8-bit output 
+	end registerB; 
+	
+architecture behavior of registerB is 
+begin 
+	process (reset, clk) 
+	begin 
+		if reset = '0' then 
+			Q <= "00000000" ;
+		elsif (clk'EVENT AND clk = '1') then 
+			Q <= B ;
+		end if ;
+	end process ; 
+end behavior; 
